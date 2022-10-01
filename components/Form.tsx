43294ca97c932015cc773e33e77509f2bf1acfe3
LordPrinz <plazinski2902@gmail.com>
2022-10-02 00:12:25 +0200

    Add input logic

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
+
 const Form = () => {
-	const formSubmitHandler = () => {};
+	const [enteredUrl, setEnteredUrl] = useState("");
+
+	const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+	};
+
+	const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setEnteredUrl(event.target.value);
+	};
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<input type="text" placeholder="link" />
+			<input
+				type="text"
+				placeholder="link"
+				value={enteredUrl}
+				onInput={inputHandler}
+			/>
 			<input type="submit" value="Skróć" />
 		</form>
 	);
