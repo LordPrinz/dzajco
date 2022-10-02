497c695de8a2eadc0fa37d3001d623c801df633f
LordPrinz <plazinski2902@gmail.com>
2022-10-02 14:08:56 +0200

    Add generateRandom util

---
+++ util/generateRandom.ts
@@ -0,0 +1,7 @@
+const generateRandom = (min: number, max: number): number => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export default generateRandom;
