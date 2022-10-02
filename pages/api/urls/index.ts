d430f91983f56e54fb23f6ba255d7e61280bb9b6
LordPrinz <plazinski2902@gmail.com>
2022-10-02 12:58:01 +0200

    Setup urls endpoints

---
+++ pages/api/urls/index.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === "POST") {
+	}
+	if (req.method === "GET") {
+	}
+}
