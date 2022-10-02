88ec5bb0ac397c4855c16cc9bcdffc21670d3abd
LordPrinz <plazinski2902@gmail.com>
2022-10-02 13:14:20 +0200

    Add custom name input

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,22 +2,54 @@ import React, { useState } from "react";
 
 const Form = () => {
 	const [enteredUrl, setEnteredUrl] = useState("");
-
-	const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+	const [customName, setCustomName] = useState("");
+	const formSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
+
+		if (!enteredUrl) {
+			return;
+		}
+
+		if (!customName) {
+			const result = await fetch("/api/urls", {
+				method: "POST",
+				body: JSON.stringify({
+					url: enteredUrl,
+				}),
+			});
+			return;
+		}
+
+		const result = await fetch("/api/urls", {
+			method: "POST",
+			body: JSON.stringify({
+				url: enteredUrl,
+				customName,
+			}),
+		});
 	};
 
-	const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const linkInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setEnteredUrl(event.target.value);
+	};
+
+	const nameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setCustomName(event.target.value);
 	};
 
 	return (
 		<form onSubmit={formSubmitHandler}>
 			<input
 				type="text"
-				placeholder="link"
+				placeholder="Link"
 				value={enteredUrl}
-				onInput={inputHandler}
+				onInput={linkInputHandler}
+			/>
+			<input
+				type="text"
+				placeholder="Custom Name"
+				value={customName}
+				onInput={nameInputHandler}
 			/>
 			<input type="submit" value="Skróć" />
 		</form>
--- pages/api/urls/index.ts
+++ pages/api/urls/index.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === "POST") {
+		console.log(req.body);
 	}
 	if (req.method === "GET") {
 	}
