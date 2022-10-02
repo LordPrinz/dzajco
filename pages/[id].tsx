6b41462caed3a4494b72b70b5188da96f460713f
LordPrinz <plazinski2902@gmail.com>
2022-10-02 16:32:46 +0200

    Implement redirecting logic

---
+++ pages/[id].tsx
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse, NextPage } from "next";
+import dbConnect from "../lib/dbConnect";
+import linkSchema from "../models/link-schema";
+
+const Page: NextPage = () => {
+	return <div>Redirecting...</div>;
+};
+
+export default Page;
+
+export async function getServerSideProps({ req, res }: { req: any; res: any }) {
+	const url = req.url.slice(1);
+
+	await dbConnect();
+
+	const link = await linkSchema.findById(url);
+
+	if (!link) {
+		return {
+			notFound: true,
+		};
+	}
+
+	await linkSchema.findOneAndUpdate({ _id: url }, { clicks: link.clicks + 1 });
+
+	return {
+		redirect: {
+			destination: link.full,
+			permanent: true,
+		},
+	};
+}
