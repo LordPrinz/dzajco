cc7f2a59dc5ce6b27301453c764eede2e0944405
LordPrinz <plazinski2902@gmail.com>
2022-10-02 13:34:57 +0200

    FIX: dbConnect

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -18,16 +18,7 @@ async function dbConnect() {
 	}
 
 	if (!cached.promise) {
-		const opts = {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			bufferCommands: false,
-			bufferMaxEntries: 0,
-			useFindAndModify: true,
-			useCreateIndex: true,
-		};
-
-		cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+		cached.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
 			return mongoose;
 		});
 	}
--- pages/api/urls/index.ts
+++ pages/api/urls/index.ts
@@ -1,9 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../../lib/dbConnect";
+import linkSchema from "../../../models/link-schema";
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	await dbConnect();
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === "POST") {
-		console.log(req.body);
+		const { body } = req;
+		console.log(body);
 	}
 	if (req.method === "GET") {
 	}
