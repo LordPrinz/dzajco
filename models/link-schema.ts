4266716faff2f878d0fd0f04a7c646ceabfb3590
LordPrinz <plazinski2902@gmail.com>
2022-10-02 13:31:12 +0200

    Update link-schema

--- models/link-schema.ts
+++ models/link-schema.ts
@@ -2,6 +2,8 @@ import mongoose, { Schema } from "mongoose";
 const LinkSchema = new Schema({
 	full: { type: String, required: true },
 	short: { type: String, required: true },
+	name: { type: String, required: false },
+	clicks: { type: Number, required: true },
 });
 
 const name = "LinkSchema";
