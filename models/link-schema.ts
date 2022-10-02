7c5ef2bf5f870c4e8762870843461ab154e4e6c3
LordPrinz <plazinski2902@gmail.com>
2022-10-02 14:45:39 +0200

    Add isValidUrl util function

--- models/link-schema.ts
+++ models/link-schema.ts
@@ -2,7 +2,6 @@ import mongoose, { Schema } from "mongoose";
 const LinkSchema = new Schema({
 	_id: { type: String, required: true },
 	full: { type: String, required: true },
-	short: { type: String, required: true },
 	clicks: { type: Number, required: true },
 });
 
--- pages/api/urls/index.ts
+++ pages/api/urls/index.ts
@@ -12,10 +12,25 @@ export default async function handler(
 	if (req.method === "POST") {
 		const { body } = req;
 
+		if (!body.url) {
+			return res.status(422).json({ message: "No full link provided." });
+		}
+
 		const cutomName = body.customName;
 
 		if (cutomName) {
 			const link = await linkSchema.findOne({ _id: cutomName });
+
+			if (link) {
+				return res.status(422).json({ message: "This name already exists." });
+			}
+
+			linkSchema.insertMany([
+				{
+					_id: cutomName,
+					full: body.url,
+				},
+			]);
 		}
 
 		// nanoid(generateRandom(4, 10));
