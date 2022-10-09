a0b2ab8cdd58747036cfddfa0f76b3b8c98e479e
LordPrinz <plazinski2902@gmail.com>
2022-10-09 14:26:18 +0200

    Style hide button

--- components/Hidden.tsx
+++ components/Hidden.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-
+import { AiFillCaretRight } from "react-icons/ai";
 const Hidden = ({ children }: { children: React.ReactNode }) => {
 	const [isShownm, setIsShown] = useState(false);
 
 	return (
 		<>
 			<button
-				className="mt-4 mb-4 ml-2 self-start px-4 py-2"
+				className="mt-4 mb-4 ml-2 self-start py-2"
 				onClick={() => {
 					setIsShown((prevState) => !prevState);
 				}}
 			>
-				Ustaw własną nazwę
+				<span className="hidden-description">
+					<AiFillCaretRight />
+					Więcej opcji
+				</span>
 			</button>
 			{isShownm && children}
 		</>
