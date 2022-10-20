90fa9d8cb133e91cb5fa91894cecaf8e0fa87965
pascal <pascal@informa.zsti>
2022-10-20 14:17:48 +0200

    Control save

--- components/LinkCopier.tsx
+++ components/LinkCopier.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { AiOutlineCopy } from "react-icons/ai";
 import copy from "../util/copy";
 
@@ -7,12 +7,21 @@ const LinkCopier = ({ url }: { url: string }) => {
     copy(`${window.location.href}${url}`);
   }, [url]);
 
+  let pop;
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("XD");
+      pop = "-translate-y-0.5 transition";
+    }, 2000);
+  }, []);
+
   return (
     <div className="notification" onClick={copyHandler}>
       <div className="notification-link">{url}</div>
       <AiOutlineCopy
         size={25}
-        className="notification-icon"
+        className={`notification-icon ${pop}`}
         onClick={copyHandler}
       />
     </div>
