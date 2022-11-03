273b796ee1115ede10c6256f2a983c93ea39deb9
LordPrinz <plazinski2902@gmail.com>
2022-11-03 21:57:07 +0100

    Change 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import image from "./../public/cracked_egg.svg";
+import image from "./../public/404.svg";
 export default function FourOhFour() {
 	return (
 		<>
 			<Head>
 				<title>404 Not Found</title>
 			</Head>
-			<div className="flex justify-center items-center h-[100vh] flex-col">
+			<div className="flex justify-center items-center h-[100vh] flex-col scale-75">
 				<Link href="/">
 					<a className="-translate-y-4">
-						<Image src={image} alt="Cracked egg image" width="460" />
+						<Image src={image} alt="Cracked egg image" width="530" />
 					</a>
 				</Link>
-				<h1 className="text-4xl mb-11  ml-10 tracking-widest">
+				<h1 className="text-5xl mb-11 translate-y-7 font-extrabold  ml-10 tracking-widest text-[#1C1C1C]">
 					Page Not Found
 				</h1>
 			</div>
