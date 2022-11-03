c06689cafef33bfa325cb0ce350c9579387e772a
LordPrinz <plazinski2902@gmail.com>
2022-11-03 21:50:53 +0100

    Add error page

---
+++ pages/_error.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import image from "./../public/cracked_egg.svg";
+import Head from "next/head";
+function Error({ statusCode }: { statusCode: number }) {
+	return (
+		<>
+			<Head>
+				<title>Something went wrong!</title>
+			</Head>{" "}
+			<div className="flex justify-center items-center h-[100vh] flex-col">
+				<Link href="/">
+					<a className="-translate-y-4">
+						<Image src={image} alt="Cracked egg image" width="460" />
+					</a>
+				</Link>
+				<h1 className="text-4xl mb-11  ml-10 tracking-widest">
+					<p>
+						{statusCode
+							? `An error ${statusCode} occurred on server`
+							: "An error occurred on client"}
+					</p>
+				</h1>
+			</div>
+		</>
+	);
+}
+
+Error.getInitialProps = ({ res, err }: any) => {
+	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+	return { statusCode };
+};
+
+export default Error;
