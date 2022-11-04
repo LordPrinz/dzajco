9dcb7c2a87e189b4e4b46873c48d033cb44c6af1
LordPrinz <61083743+LordPrinz@users.noreply.github.com>
2022-11-04 12:14:26 +0100

    Update stats.tsx

--- pages/[id]/stats.tsx
+++ pages/[id]/stats.tsx
@@ -1,37 +1,43 @@
-import type { NextPage } from "next";
-import dbConnect from "../../lib/dbConnect";
-import linkSchema from "../../models/link-schema";
-
+import type { NextPage } from 'next';
+import dbConnect from '../../lib/dbConnect';
+import linkSchema from '../../models/link-schema';
+import { SiEgghead } from 'react-icons/si';
 const StatsPage: NextPage = (props: any) => {
-	return (
-		<>
-			<div>
-				<div>Clicks</div>
-				<div>{props.clicks}</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh] flex-col">
+        <div className="relative">
+          <h1 className="text-6xl p-32 border-8 border rounded-full">
+            {props.clicks || 0}
+          </h1>
+          <span className="absolute top-0 left-1/2 -translate-x-1/2 z-10 bg-[#eee] px-4 -translate-y-4 text-4xl text-[#909296]">
+            <SiEgghead />
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default StatsPage;
 
 export async function getServerSideProps({ req }: { req: any }) {
-	const url = req.url.slice(1).split("/")[0];
+  const url = req.url.slice(1).split('/')[0];
 
-	await dbConnect();
+  await dbConnect();
 
-	const link = await linkSchema.findById(url);
+  const link = await linkSchema.findById(url);
 
-	if (!link) {
-		return {
-			notFound: true,
-		};
-	}
+  if (!link) {
+    return {
+      notFound: true,
+    };
+  }
 
-	return {
-		props: {
-			clicks: link.clicks,
-			page: url,
-		},
-	};
+  return {
+    props: {
+      clicks: link.clicks,
+      page: url,
+    },
+  };
 }
