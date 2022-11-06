8d5c49ba894967225f6c556c3a9b4095d6923aa7
LordPrinz <plazinski2902@gmail.com>
2022-11-06 21:26:26 +0100

    Improve error page code quality

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -3,30 +3,36 @@ import Image from "next/image";
 import image from "./../public/cracked_egg.svg";
 import Head from "next/head";
 import logo from "./../public/logo.svg";
-function Error({ statusCode }: { statusCode: number }) {
+import { NextPage } from "next";
+
+type Props = {
+	statusCode: number;
+};
+
+const Error: NextPage<Props> = ({ statusCode }) => {
+	const errorMessage = statusCode
+		? `An error ${statusCode} occurred on server`
+		: "An error occurred on client";
+
 	return (
 		<>
 			<Head>
 				<title>Something went wrong!</title>
 				<link rel="icon" type="image/x-icon" href={logo} />
 			</Head>
-			<div className="flex justify-center items-center h-[100vh] flex-col">
+			<div className="error__container">
 				<Link href="/">
-					<a className="-translate-y-4">
+					<a className="error__image">
 						<Image src={image} alt="Cracked egg image" width="460" />
 					</a>
 				</Link>
-				<h1 className="text-4xl mb-11  ml-10 tracking-widest">
-					<p>
-						{statusCode
-							? `An error ${statusCode} occurred on server`
-							: "An error occurred on client"}
-					</p>
+				<h1 className="error__title">
+					<p>{errorMessage}</p>
 				</h1>
 			</div>
 		</>
 	);
-}
+};
 
 Error.getInitialProps = ({ res, err }: any) => {
 	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
