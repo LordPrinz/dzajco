57db3a131062e2125b78fbe52654c05b253b36ae
LordPrinz <plazinski2902@gmail.com>
2023-07-05 00:30:40 +0200

    Create handleRateLimiter

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,24 @@
+import { limiter } from "@/app/api/urls/config/limiter";
+import { NextResponse } from "next/server";
+
 export const sendWrongInputResponse = (message: string) => {
-	return new Response(message, {
+	return new NextResponse(message, {
 		status: 422,
 	});
+};
+
+export const handleRateLimiter = async (request: Request) => {
+	const origin = request.headers.get("origin");
+	const remaining = await limiter.removeTokens(1);
+
+	if (remaining < 0) {
+		return new NextResponse(null, {
+			status: 429,
+			statusText: "Too Many Requests",
+			headers: {
+				"Access-Control-Allow-Origin": origin || "*",
+				"Content-Type": "text/plain",
+			},
+		});
+	}
 };
