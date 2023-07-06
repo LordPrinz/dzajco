c616f71e7df8e688fad24311b132a951d01c8b4a
LordPrinz <plazinski2902@gmail.com>
2023-07-06 13:01:35 +0200

    Add dynamic redirects

--- src/app/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-	return (
-		<footer className="pb-1 flex items-end place-self-center flex-1 pt-1 mt-3 flex-wrap"></footer>
-	);
-};
-
-export default Footer;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import LinksList from "./LinksList";
+
+const Footer = () => {
+	return (
+		<footer className="pb-1 flex flex-1 pt-1 mt-3 flex-wrap justify-center">
+			<LinksList />
+		</footer>
+	);
+};
+
+export default Footer;
