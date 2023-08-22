95745746497a5faedf85ce97aee9dd1d3052832c
LordPrinz <plazinski2902@gmail.com>
2023-08-23 00:41:47 +0200

    Style header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
 	return (
 		<header className="flex flex-col items-center pt-16 pb-3">
-			<h1 className="text-5xl pb-7 font-bold text-gray-900">dzajco</h1>
+			<h1 className="text-5xl pb-7 font-bold text-gray-800">dzajco</h1>
 			<h2 className="text-xl inline-block text-center text-gray-500">
 				The best link shortener you were looking for.
 			</h2>
