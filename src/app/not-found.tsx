adb82e69926809b641d94c765c92c3d168692660
LordPrinz <plazinski2902@gmail.com>
2023-08-31 19:05:16 +0200

    Fix 404 page colors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -22,7 +22,7 @@ const NotFoundPage = () => {
 					<Image src="/404.svg" alt="Cracked egg image" fill={true} />
 				</div>
 				<h3 className="text-4xl font-extrabold  tracking-widest text-[#1C1C1C] text-center ">
-					No i co? <i>Dżajco</i>.
+					No i co? <span className="text-jajco-500 px-1">Dżajco</span>
 				</h3>
 			</Link>
 		</main>
