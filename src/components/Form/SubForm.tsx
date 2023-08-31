77fb0b51720dcae974249480f8198b177c0cff4f
LordPrinz <plazinski2902@gmail.com>
2023-08-31 18:38:25 +0200

    Fix hidden component submitting form

--- src/components/Form/SubForm.tsx
+++ src/components/Form/SubForm.tsx
@@ -6,8 +6,10 @@ import Input from "./Input";
 const SubForm = () => {
 	const { value, setValue } = useInput({});
 
+	const submitHandler = () => {};
+
 	return (
-		<form className="max-w-3xl mx-auto">
+		<form className="max-w-3xl mx-auto" onSubmit={submitHandler}>
 			<h3 className="inline-block text-sm text-center text-gray-500 w-full mb-7 border-b pb-0.5">
 				<div className="z-10 translate-y-1/2 px-2 bg-gray-50 inline-block">
 					Link statistics
--- src/components/shared/Hidden.tsx
+++ src/components/shared/Hidden.tsx
@@ -21,14 +21,15 @@ const Hidden = ({ children, text, className }: Props) => {
 
 	return (
 		<>
-			<div
+			<button
+				type="button"
 				className={cn("self-start py-2 cursor-pointer inline-block", className)}
 				onClick={hiddenButtonHandler}>
 				<span className="flex items-center gap-1.5 text-sm">
 					<AiFillCaretRight className={`${arrowRotate} transition`} />
 					{text}
 				</span>
-			</div>
+			</button>
 			{isShown && children}
 		</>
 	);
