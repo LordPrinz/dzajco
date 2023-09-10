e3d196f75f0ea3d780edc7c707981fb9ef081250
LordPrinz <plazinski2902@gmail.com>
2023-09-10 23:43:47 +0200

    Add responsive to history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -7,7 +7,7 @@ const HistoryPage = () => {
 	return (
 		<div className="col-[center-start/center-end]">
 			<HomeButton />
-			<h1 className="text-center mt-14 text-4xl font-bold text-jajco-500">
+			<h1 className="text-center mt-14 text-3xl md:text-4xl font-bold text-jajco-500 ">
 				Links History
 			</h1>
 			<History action={removeItem} />
--- src/components/shared/HomeButton.tsx
+++ src/components/shared/HomeButton.tsx
@@ -7,8 +7,8 @@ const HomeButton = () => {
 		<Link
 			className="flex text-[#1C1C1C] font-bold absolute text-4xl top-5 left-4 gap-1.5  justify-center px-2 py-1"
 			href="/">
-			<GoHomeFill className="text-2xl" />{" "}
-			<span className="text-lg -translate-y-0.5">Home</span>
+			<GoHomeFill className="text-xl md:text-2xl" />
+			<span className="text-base md:text-lg -translate-y-0.5">Home</span>
 		</Link>
 	);
 };
