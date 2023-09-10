d850b454045ce11e4d1079625d56b920fa4b934d
LordPrinz <plazinski2902@gmail.com>
2023-09-10 19:13:35 +0200

    Set about page as coming soon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
+import HomeButton from "@/components/shared/HomeButton";
+
 const AboutPage = () => {
-	return <div>AboutPage</div>;
+	return (
+		<div className="col-[center-start/center-end] text-jajco-500 text-4xl text-center mt-16">
+			<HomeButton />
+			Coming soon...
+		</div>
+	);
 };
 
 export default AboutPage;
