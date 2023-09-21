6bb9d7f23df1f4b8467b5b25a9c3678e9c2e2896
LordPrinz <61083743+LordPrinz@users.noreply.github.com>
2023-09-21 13:15:22 +0200

    Update removeItem.ts

--- src/app/history/_actions/removeItem.ts
+++ src/app/history/_actions/removeItem.ts
@@ -5,6 +5,11 @@ import dbConnect from "@/utils/db";
 import { revalidatePath } from "next/cache";
 
 export const removeItem = async (id: string, secretKey: string) => {
+
+	if(!secretKey) {
+		return;
+	}
+	
 	await dbConnect();
 
 	await LinkModel.deleteOne({
