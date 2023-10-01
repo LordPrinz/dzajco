adc39b64006f838f76a7fca3ea0245c1368b3b35
LordPrinz <plazinski2902@gmail.com>
2023-10-01 21:47:52 +0200

    Create

--- src/app/dzajapi/_components/ApiElement.tsx
+++ src/app/dzajapi/_components/ApiElement.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ApiMethod from "./ApiMethod";
 
 type Props = {
@@ -9,18 +11,26 @@ type Props = {
 };
 
 const ApiElement = ({ name, endpoint, method, children }: Props) => {
+	const [visible, setVisible] = useState(false);
+
+	const toggle = () => {
+		setVisible((state) => !state);
+	};
+
 	return (
-		<div className="w-full flex flex-col text-xl">
+		<div className="w-full flex flex-col text-xl" onClick={toggle}>
 			<p className="text-xl text-gray-800 text-left">{name}</p>
 			<div className="mt-1.5">
 				<div className="rounded-md flex items-center gap-4 bg-gray-100">
 					<ApiMethod method={method} />
 					<div className="text-gray-500">{endpoint}</div>
 				</div>
-				<div className="p-2 text-lg space-y-3">
-					<p className="text-gray-600 mt-2 text-left">Response</p>
-					{children}
-				</div>
+				{visible && (
+					<div className="p-2 text-lg space-y-3">
+						<p className="text-gray-600 mt-2 text-left">Response</p>
+						{children}
+					</div>
+				)}
 			</div>
 		</div>
 	);
