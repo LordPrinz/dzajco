c1b50a29bcfd49f99e0b8c690fef53f329d47cad
LordPrinz <plazinski2902@gmail.com>
2023-10-01 15:04:58 +0200

    Create ApiElement

---
+++ src/app/dzajapi/_components/ApiElement.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const apiElement = () => {
+	return <div>apiElement</div>;
+};
+
+export default apiElement;
--- src/app/dzajapi/page.tsx
+++ src/app/dzajapi/page.tsx
@@ -4,7 +4,6 @@ const Page = () => {
 	return (
 		<div className="col-[center-start/center-end] text-jajco-500 text-4xl text-center mt-16">
 			<HomeButton />
-			Coming soon...
 		</div>
 	);
 };
