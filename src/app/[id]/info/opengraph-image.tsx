86b70c6a9c475bdcfa5ee6d130d4c95167e13fa8
LordPrinz <plazinski2902@gmail.com>
2023-10-04 20:33:45 +0200

    Add logo

--- src/app/[id]/info/opengraph-image.tsx
+++ src/app/[id]/info/opengraph-image.tsx
@@ -4,30 +4,38 @@ import { ImageResponse } from "next/server";
 import dbConnect, { findLink } from "@/utils/db";
 
 export const size = {
-  width: 1200,
-  height: 630,
+	width: 1200,
+	height: 630,
 };
 
 export const contentType = "image/png";
 
 export default async function og({ params }: Props) {
-  const { id } = params;
+	const { id } = params;
 
-  //   await dbConnect();
+	//   await dbConnect();
 
-  //   const link = await findLink({ id: params.id });
+	//   const link = await findLink({ id: params.id });
 
-  //   if (!link) {
-  //     return new ImageResponse(
-  //       <div tw="relative flex w-full h-full">Not found!</div>
-  //     );
-  //   }
+	//   if (!link) {
+	//     return new ImageResponse(
+	//       <div tw="relative flex w-full h-full">Not found!</div>
+	//     );
+	//   }
 
-  return new ImageResponse(
-    (
-      <div tw="relative w-full h-full flex">
-        Dzajco/<span>{id}</span>
-      </div>
-    )
-  );
+	return new ImageResponse(
+		(
+			<div tw="relative w-full h-full flex">
+				<div tw="flex">
+					<div tw="flex">
+						Dzajco/<span>{id}</span>
+						<div></div>
+					</div>
+					<div tw="flex">
+						<img src="https://www.dzaj.de/logo.svg" width={100} height={100} />
+					</div>
+				</div>
+			</div>
+		)
+	);
 }
