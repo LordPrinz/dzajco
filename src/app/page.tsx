a8ae1e4c3eb10b415ff453b3facbd2959cd2cdcf
LordPrinz <plazinski2902@gmail.com>
2023-10-26 00:03:55 +0200

    Add dzajnniversary event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/components/Layout/Header";
 import Forms from "./_components/Forms";
+import ConffetiContainer from "@/components/Events/ConffetiContainer";
 
 export default function Home() {
 	return (
-		<main className="col-[center-start/center-end]">
-			<Header />
-			<Forms />
-		</main>
+		<>
+			<ConffetiContainer />
+			<main className="col-[center-start/center-end]">
+				<Header />
+				<Forms />
+			</main>
+		</>
 	);
 }
---
+++ src/components/Events/ConffetiContainer.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { cn } from "@/utils/tailwind";
+import React, { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+
+const ConffetiContainer = () => {
+	const [isRendered, setIsRendered] = useState(false);
+
+	const [isConffeti, setIsConffeti] = useState(true);
+
+	const [fadeOutConffeti, setFadeOutConffeti] = useState(false);
+
+	useEffect(() => {
+		setIsRendered(true);
+	}, []);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsConffeti(false);
+		}, 6000);
+	}, []);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setFadeOutConffeti(true);
+		}, 5000);
+	});
+
+	return (
+		<>
+			{isRendered && isConffeti && (
+				<>
+					<Confetti
+						className={cn(
+							"opacity-100 transition duration-1000",
+							fadeOutConffeti ? "opacity-0 transition" : ""
+						)}
+						width={window.innerWidth}
+					/>
+					<div
+						className={cn(
+							"absolute -ml-40 mt-1 top-44 left-1/2  text-3xl -rotate-12 font-bold text-jajco-500",
+							"transition duration-1000 animate-bounce",
+							fadeOutConffeti
+								? "opacity-0 transition"
+								: "opacity-100 transition duration-1000"
+						)}>
+						Happy dzajnniversary!
+					</div>
+				</>
+			)}
+		</>
+	);
+};
+
+export default ConffetiContainer;
