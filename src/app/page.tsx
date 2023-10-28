6ae128e5d8e5be41d822397c7286e5cf2e4b25f7
LordPrinz <plazinski2902@gmail.com>
2023-10-28 20:25:59 +0200

    Remove conffeti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Layout/Header";
 import Forms from "./_components/Forms";
-import ConffetiContainer from "@/components/Events/ConffetiContainer";
+// import ConffetiContainer from "@/components/Events/ConffetiContainer";
 
 export default function Home() {
 	return (
 		<>
-			<ConffetiContainer />
+			{/* <ConffetiContainer /> */}
 			<main className="col-[center-start/center-end]">
 				<Header />
 				<Forms />
