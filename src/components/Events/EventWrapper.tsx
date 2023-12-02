3de7855e0a42ca2c99689a72e82bc49e1f3ea5d2
LordPrinz <plazinski2902@gmail.com>
2023-12-02 21:58:17 +0100

    Add fireworks

--- src/components/Events/EventWrapper.tsx
+++ src/components/Events/EventWrapper.tsx
@@ -1,9 +1,15 @@
+import Fireworks from "@fireworks-js/react";
 import ConffetiContainer from "./ConffetiContainer";
+import Snowfall from "./Snowfall";
 
 const EventWrapper = () => {
 	let event = null;
 
-	event = <ConffetiContainer />;
+	// event = <ConffetiContainer />;
+
+	event = <Snowfall />;
+
+	event = <Fireworks className="absolute w-full h-full" />;
 
 	return <> {event} </>;
 };
