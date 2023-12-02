c054e09041706ffbc8b0c73b2de1d875f75173fd
LordPrinz <plazinski2902@gmail.com>
2023-12-02 16:12:30 +0100

    Create EventWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Layout/Header";
 import Forms from "./_components/Forms";
+import EventWrapper from "@/components/Events/EventWrapper";
 // import ConffetiContainer from "@/components/Events/ConffetiContainer";
 
 export default function Home() {
 	return (
 		<>
-			{/* <ConffetiContainer /> */}
+			<EventWrapper />
 			<main className="col-[center-start/center-end]">
 				<Header />
 				<Forms />
---
+++ src/components/Events/EventWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EventWrapper = () => {
+	let event = null;
+
+	return <> event </>;
+};
+
+export default EventWrapper;
