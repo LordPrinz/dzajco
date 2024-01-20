7fc1f73e8f902225cada5fb146a8226482503f4f
LordPrinz <plazinski2902@gmail.com>
2024-01-20 23:56:18 +0100

    Create about section

--- src/app/_components/About.tsx
+++ src/app/_components/About.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Feature from "./Feature";
-import { MdLogin } from "react-icons/md";
+import {
+	MdBrush,
+	MdDelete,
+	MdHistory,
+	MdLocationPin,
+	MdOutlineLogin,
+	MdOutlineTimelapse,
+} from "react-icons/md";
 
 const About = () => {
 	return (
-		<section className="mt-44 flex flex-col">
+		<section className="mt-52 flex flex-col">
 			<h3 className="text-3xl  font-bold text-gray-00 text-center border-b pb-0 mb-12 max-w-3xl self-center w-full">
 				<div className="translate-y-4 bg-light-bg-primary inline-block px-3 ">
 					Why you should use <span className="text-jajco-500"> Dzajco</span>
@@ -18,11 +25,36 @@ const About = () => {
 					professionals alike.
 				</p>
 			</div>
-			<div className="mt-16 grid grid-cols-4 gap-4">
+			<div className="my-16 grid sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-3 gap-4">
 				<Feature
-					icon={<MdLogin />}
-					title="No login"
+					icon={<MdOutlineLogin />}
+					title="No login required"
 					description="Forget the hassle of creating accounts. With dzajco, you can shorten your links instantly without the need for a login."
+				/>
+				<Feature
+					icon={<MdOutlineTimelapse />}
+					title="Link Expiration"
+					description="Take control of your links' lifespan. Set expiration dates to add an extra layer of security, perfect for time-sensitive promotions or temporary sharing needs.   "
+				/>
+				<Feature
+					icon={<MdBrush />}
+					title="Customized Link Names"
+					description="Personalize your links with custom names or let our system auto-generate unique identifiers, ensuring your content stays uniquely yours."
+				/>
+				<Feature
+					icon={<MdDelete />}
+					title="Manual Link Deletion"
+					description="Retract links at your discretion. Dzajco allows you to delete links manually, giving you full control over your content and its accessibility.          "
+				/>
+				<Feature
+					icon={<MdLocationPin />}
+					title="Visitor Tracking"
+					description="Knowledge is power, especially when it comes to security. Track the number of visitors and their geographic locations effortlessly to stay informed about who engages with your content.          "
+				/>
+				<Feature
+					icon={<MdHistory />}
+					title="Link History"
+					description="Keep your link management organized and secure with our comprehensive link history. Easily revisit, revise, or delete links as needed, knowing your content is in safe hands."
 				/>
 			</div>
 		</section>
--- src/app/_components/Feature.tsx
+++ src/app/_components/Feature.tsx
@@ -6,8 +6,16 @@ type FeatureProps = {
 	description: string;
 };
 
-const Feature = ({}: FeatureProps) => {
-	return <div className="p-4 border shadow-xs rounded-md">Feature</div>;
+const Feature = ({ icon, title, description }: FeatureProps) => {
+	return (
+		<div className="p-4 flex gap-3 items-start space-x-1">
+			<span className="self-start text-jajco-500 text-4xl mt-1">{icon}</span>
+			<div className="space-y-2">
+				<h4 className="text-lg font-bold text-gray-700">{title}</h4>
+				<p className="text-gray-500 text-sm leading-[21px]">{description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Feature;
