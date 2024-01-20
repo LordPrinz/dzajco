eda78490815b6f2a0c2f7eb7d43f0d9901d52f51
LordPrinz <plazinski2902@gmail.com>
2024-01-20 22:59:44 +0100

    Add different Footer position

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import LinksList from "./LinksList";
 
 const Footer = () => {
 	return (
-		<footer className="pb-1 flex flex-1 items-center pt-1 mt-3 flex-wrap justify-center col-[full-start/full-end]">
+		<footer className="pb-1 flex flex-1 items-center mt-3 flex-wrap justify-center col-[full-start/full-end] z-[9999] fixed bottom-0 bg-light-bg-primary opacity-80 w-full pt-5">
 			<LinksList />
 		</footer>
 	);
