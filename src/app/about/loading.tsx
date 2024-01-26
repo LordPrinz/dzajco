043ed639b04d6d17583570ebd591b8f8e43e9a7d
LordPrinz <plazinski2902@gmail.com>
2024-01-26 19:17:02 +0100

    Change main page color

--- src/app/about/loading.tsx
+++ src/app/about/loading.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Counter from "../[id]/info/_components/Counter";
 import HomeButton from "@/components/shared/HomeButton";
+import About from "./_components/About";
 
 const Loading = () => {
 	return (
-		<main className="col-[center-start/center-end] mt-4">
+		<main className="col-[center-start/center-end]">
 			<HomeButton />
-			<section className="grid gap-2 sm:grid-cols-2  mt-12 grid-rows-[min-content,min-content,min-content,1fr]">
+			<About />
+			<section className="grid gap-2 sm:grid-cols-2  mt-10 grid-rows-[min-content,min-content,min-content,1fr]">
 				<div className="info-panel py-4 flex flex-col items-center justify-center h-full">
 					<h3 className="font-bold text-3xl text-center mt-12">Shorten Links</h3>
 					<Counter visits={0} className="flex-1 flex my-12 justify-center mt-9 " />
