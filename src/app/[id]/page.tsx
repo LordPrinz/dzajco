c8a196a318e53a4a498bcc8d78aa965b502419f6
LordPrinz <plazinski2902@gmail.com>
2024-04-13 17:19:46 +0200

    Add metadata display to links

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -8,6 +8,8 @@ import { notFound, redirect } from "next/navigation";
 import { headers } from "next/headers";
 import { getUserLocation } from "@/utils/utils";
 import RedirectPage from "./info/_components/RedirectPage";
+import axios from "axios";
+import cheerio from "cheerio";
 
 type Props = {
 	params: { id: string };
@@ -35,7 +37,26 @@ export async function generateMetadata({ params }: Props) {
 		redirect(fullLink);
 	}
 
-	//TODO: Add a metadata from a target page!
+	try {
+		const response = await axios.get(fullLink);
+		const html = response.data;
+		const $ = cheerio.load(html);
+
+		const title = $("title").text();
+		const description = $('meta[name="description"]').attr("content");
+		let image = $('meta[property="og:image"]').attr("content");
+		if (!image) {
+			image = $("img").first().attr("src");
+		}
+		return {
+			title: title || "No Title",
+			description: description || "No Description",
+			image: image || null,
+			// Add other metadata fields here
+		};
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 const Page: NextPage<Props> = async ({ params }) => {
@@ -43,8 +64,6 @@ const Page: NextPage<Props> = async ({ params }) => {
 	const link = await findLink({ id: params.id });
 
 	const fullLink = link!.full.replaceAll("​", "");
-
-	console.log(fullLink.length);
 
 	try {
 		const ip = headers().get("x-forwarded-for")!;
